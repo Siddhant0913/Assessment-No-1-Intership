9f488779509e1ec3517957e07f7f758267ac9119
Siddhant Waghmare <116907096+Siddhant0913@users.noreply.github.com>
2023-02-23 18:37:38 +0530

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,25 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'siddhantv465@gmail.com';
+
+    $email_subject = 'New Form Submission';
+
+    $email_body = "User Name: $name.\n",
+                  "User Email: $visitor_email.\n"
+                  "User Message: $message.\n";
+
+        $to = "siddhantv1309@gmail.com";
+
+        $headers = "From: $email_from \r\n";
+
+        $headers = "Reply-To: $visitor_email \r\n";
+
+        mail($to,$email_subject,$email_body,$headers);
+
+        header("Location: index.html");
+
+
+?>
